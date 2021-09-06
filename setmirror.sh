13ac3eac75e363e149ee77fd80e6a102068f1813
Andrea Zuccherelli <andrea@zuccherelli.net>
2021-09-06 21:25:19 +0200

    setmirror.sh

---
+++ setmirror.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# https://serverfault.com/a/528605
+echo "Start ubuntu.sh runscript" >> /var/log/syslog
+for x in `cat /proc/cmdline`; do
+        case $x in RPHOST*)
+                eval $x
+                HOST=$RPHOST
+                echo "d-i mirror/http/hostname string ${HOST}" > /tmp/mirror.cfg
+                echo "d-i mirror/http/mirror string ${HOST}" >> /tmp/mirror.cfg
+                echo "d-i apt-setup/security_host string ${HOST}" >> /tmp/mirror.cfg
+                ;;
+        esac;
+done
+# add's values to /var/lib/cdebconf/question.dat
+debconf-set-selections /tmp/mirror.cfg
