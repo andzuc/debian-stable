7b0a02bb82a38b1b67d82a69b0a45a466e1d652e
Andrea Zuccherelli <andrea@zuccherelli.net>
2023-10-15 20:22:41 +0200

    vagrant box gen

--- preseed/customize.sh
+++ preseed/customize.sh
@@ -2,12 +2,13 @@
 # credit https://stackoverflow.com/a/246128
 MYDIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &>/dev/null && pwd)
 export PATH="$PATH:$MYDIR"
+cd "$NYDIR"
 
 # https://www.engineyard.com/blog/building-a-vagrant-box-from-start-to-finish/
 # no password for vagrant user sudo
 echo "vagrant ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers.d/vagrant
 
-sshsetup \
+sshsetup.sh \
     /home/vagrant \
     "$(cat vagrant.pub)" \
     vagrant vagrant
