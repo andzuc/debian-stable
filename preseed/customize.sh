64971337af73b67139d50b74ecf41fb8eb573834
Andrea Zuccherelli <andrea@zuccherelli.net>
2023-10-15 20:22:41 +0200

    vagrant box gen

--- preseed/customize.sh
+++ preseed/customize.sh
@@ -4,6 +4,9 @@ MYDIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &>/dev/null && pwd)
 export PATH="$PATH:$MYDIR"
 cd "$MYDIR"
 
+DEBIAN_OS_VERSION="$(cat /etc/debian_version)"
+echo DEBIAN_OS_VERSION="${DEBIAN_OS_VERSION}"
+
 # https://www.engineyard.com/blog/building-a-vagrant-box-from-start-to-finish/
 # no password for vagrant user sudo
 echo "vagrant ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers.d/vagrant
