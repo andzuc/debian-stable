17dbbf4a7c2268baed588ddd695dbbce588eaed2
Andrea Zuccherelli <andrea@zuccherelli.net>
2024-01-10 20:25:28 +0100

    multi disk

--- preseed/early_partman.sh
+++ preseed/early_partman.sh
@@ -3,3 +3,6 @@ SYSHD_DISKS="$(list-devices disk)"
 echo SYSHD_DISKS="${SYSHD_DISKS}"
 debconf-set partman-auto/disk "${SYSHD_DISKS}"
 /mk_partman "${SYSHD_DISKS}" >partman.cfg
+
+echo PARTMAN configuration:
+cat partman.cfg
