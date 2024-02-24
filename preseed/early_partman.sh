810f6e8c8583d230ac5cadd1bfcbe759a4e01fda
Andrea Zuccherelli <andrea@zuccherelli.net>
2024-02-24 22:29:15 +0100

    export kernel/initrd

--- preseed/early_partman.sh
+++ preseed/early_partman.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-list-devices disk
-SYSHD_DISKS="$(echo $(list-devices disk)|tr -d '\n')"
-echo SYSHD_DISKS="${SYSHD_DISKS}"
+SYSHD_DISKS=
 debconf-set partman-auto/disk "${SYSHD_DISKS}"
 /mk_partman "${SYSHD_DISKS}" >partman.cfg
 
