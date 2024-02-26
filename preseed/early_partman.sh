2d0c1fda3aad60bc21cb969c9e53aca44bfae903
Andrea Zuccherelli <andrea@zuccherelli.net>
2024-02-27 00:17:49 +0100

    Revert "export kernel/initrd"
    This reverts commit 810f6e8c8583d230ac5cadd1bfcbe759a4e01fda.

--- preseed/early_partman.sh
+++ preseed/early_partman.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-echo ">>> BLOCKDEVS"
-ls -l /sys/block/*
-echo "<<< BLOCKDEVS"
-SYSHD_DISKS=
+list-devices disk
+SYSHD_DISKS="$(echo $(list-devices disk)|tr -d '\n')"
+echo SYSHD_DISKS="${SYSHD_DISKS}"
 debconf-set partman-auto/disk "${SYSHD_DISKS}"
 /mk_partman "${SYSHD_DISKS}" >partman.cfg
 
