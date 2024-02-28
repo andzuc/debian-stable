e7e5e9b229c67a5104f25bebb7b47e8fcf5af201
Andrea Zuccherelli <andrea@zuccherelli.net>
2024-02-28 21:00:58 +0100

    test

--- preseed/early_partman.sh
+++ preseed/early_partman.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-echo ">>> LS"
-ls /sys/block
-echo "<<< LS"
 SYSHD_DISKS="$(echo $(list-devices disk)|tr -d '\n')"
 echo SYSHD_DISKS="${SYSHD_DISKS}"
 debconf-set partman-auto/disk "${SYSHD_DISKS}"
